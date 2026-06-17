dd59b452a3e73679637288cb1e3e74a5352de6fe
AnjanaShakuntha <ashakuntha@gmail.com>
2026-06-17 20:50:57 +0530

    Add UserService class for user management functionality

---
+++ job-portal-system/src/main/java/com/jobportal/service/UserService.java
@@ -0,0 +1,31 @@
+package com.jobportal.service;
+
+import com.jobportal.entity.User;
+import com.jobportal.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public boolean emailExists(String email) {
+        return userRepository.findByEmail(email).isPresent();
+    }
+}
